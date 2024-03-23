2587cc65d0f67c3ecf80ddad51286e26df321571
Danial Raza <danialrazafb@gmail.com>
2024-03-23 10:55:29 +0100

    refactor: remove unnecessary try-catches

--- src/buttons/delete-bookmark.ts
+++ src/buttons/delete-bookmark.ts
@@ -15,29 +15,20 @@ type DeleteBookmarkButtonOptions = {
 export default {
   customId: "delete-bookmark",
   async execute({ api, env, interaction }: DeleteBookmarkButtonOptions) {
-    try {
-      const userId = interaction.user?.id ?? interaction.member!.user.id;
+    const userId = interaction.user?.id ?? interaction.member!.user.id;
 
-      if (userId !== extractCreatorIdFromBookmark(interaction.message)) {
-        return respond({
-          data: { content: "You can't delete a bookmark you didn't create.", flags: MessageFlags.Ephemeral },
-          type: InteractionResponseType.ChannelMessageWithSource,
-        });
-      }
-
-      await api.channels.deleteMessage(env.BOOKMARKS_CHANNEL_ID, interaction.message.id);
-
+    if (userId !== extractCreatorIdFromBookmark(interaction.message)) {
       return respond({
-        data: { content: "Bookmark successfully deleted.", flags: MessageFlags.Ephemeral },
-        type: InteractionResponseType.ChannelMessageWithSource,
-      });
-    } catch (error) {
-      const message = ["An error occurred while deleting the bookmark:", `\`\`\`${error}\`\`\``].join("\n");
-
-      return respond({
-        data: { content: message, flags: MessageFlags.Ephemeral },
+        data: { content: "You can't delete a bookmark you didn't create.", flags: MessageFlags.Ephemeral },
         type: InteractionResponseType.ChannelMessageWithSource,
       });
     }
+
+    await api.channels.deleteMessage(env.BOOKMARKS_CHANNEL_ID, interaction.message.id);
+
+    return respond({
+      data: { content: "Bookmark successfully deleted.", flags: MessageFlags.Ephemeral },
+      type: InteractionResponseType.ChannelMessageWithSource,
+    });
   },
 } as const;
--- src/commands/create-bookmark.ts
+++ src/commands/create-bookmark.ts
@@ -24,108 +24,99 @@ export default {
     dm_permission: false,
   },
   async execute({ api, env, interaction }: CreateBookmarkCommandOptions): Promise<Response> {
-    try {
-      const bookmarksChannel = await api.channels.get(env.BOOKMARKS_CHANNEL_ID);
+    const bookmarksChannel = await api.channels.get(env.BOOKMARKS_CHANNEL_ID);
 
-      if (bookmarksChannel.type !== ChannelType.GuildText) {
-        return respond({
-          data: { content: "The bookmarks channel is not a text channel.", flags: MessageFlags.Ephemeral },
-          type: InteractionResponseType.ChannelMessageWithSource,
-        });
-      }
-
-      const bookmarks = await api.channels.getMessages(bookmarksChannel.id, { limit: 100 });
-
-      const message = interaction.data.resolved.messages[interaction.data.target_id];
-
-      for (const bookmark of bookmarks) {
-        const linkButton = bookmark.components?.[0]?.components[0] as APIButtonComponentWithURL;
-
-        if (linkButton.url === messageLink(message.channel_id, message.id, interaction.guild_id)) {
-          return respond({
-            data: { content: "A bookmark for that message already exists.", flags: MessageFlags.Ephemeral },
-            type: InteractionResponseType.ChannelMessageWithSource,
-          });
-        }
-      }
-
+    if (bookmarksChannel.type !== ChannelType.GuildText) {
       return respond({
-        type: InteractionResponseType.Modal,
-        data: {
-          custom_id: "create-bookmark",
-          title: "Create Bookmark",
-          components: [
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  custom_id: "name",
-                  label: "Name",
-                  max_length: 256,
-                  min_length: 1,
-                  placeholder: "The name for the bookmark",
-                  required: false,
-                  style: TextInputStyle.Short,
-                  type: ComponentType.TextInput,
-                },
-              ],
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  custom_id: "description",
-                  label: "Description",
-                  max_length: 1_024,
-                  min_length: 1,
-                  placeholder: "The description for the bookmark",
-                  required: false,
-                  style: TextInputStyle.Paragraph,
-                  type: ComponentType.TextInput,
-                },
-              ],
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  custom_id: "links",
-                  label: "Links",
-                  max_length: 1_024,
-                  min_length: 1,
-                  placeholder: "The links for the bookmark separated by a space",
-                  required: false,
-                  style: TextInputStyle.Short,
-                  type: ComponentType.TextInput,
-                },
-              ],
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  custom_id: "target-message",
-                  label: "Target Message",
-                  min_length: 16,
-                  max_length: 20,
-                  placeholder: "The id of the target message",
-                  required: true,
-                  style: TextInputStyle.Short,
-                  type: ComponentType.TextInput,
-                  value: message.id,
-                },
-              ],
-            },
-          ],
-        },
-      });
-    } catch (error) {
-      const message = ["An error occurred while creating the bookmark modal:", `\`\`\`${error}\`\`\``].join("\n");
-
-      return respond({
-        data: { content: message, flags: MessageFlags.Ephemeral },
+        data: { content: "The bookmarks channel is not a text channel.", flags: MessageFlags.Ephemeral },
         type: InteractionResponseType.ChannelMessageWithSource,
       });
     }
+
+    const bookmarks = await api.channels.getMessages(bookmarksChannel.id, { limit: 100 });
+
+    const message = interaction.data.resolved.messages[interaction.data.target_id];
+
+    for (const bookmark of bookmarks) {
+      const linkButton = bookmark.components?.[0]?.components[0] as APIButtonComponentWithURL;
+
+      if (linkButton.url === messageLink(message.channel_id, message.id, interaction.guild_id)) {
+        return respond({
+          data: { content: "A bookmark for that message already exists.", flags: MessageFlags.Ephemeral },
+          type: InteractionResponseType.ChannelMessageWithSource,
+        });
+      }
+    }
+
+    return respond({
+      type: InteractionResponseType.Modal,
+      data: {
+        custom_id: "create-bookmark",
+        title: "Create Bookmark",
+        components: [
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                custom_id: "name",
+                label: "Name",
+                max_length: 256,
+                min_length: 1,
+                placeholder: "The name for the bookmark",
+                required: false,
+                style: TextInputStyle.Short,
+                type: ComponentType.TextInput,
+              },
+            ],
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                custom_id: "description",
+                label: "Description",
+                max_length: 1_024,
+                min_length: 1,
+                placeholder: "The description for the bookmark",
+                required: false,
+                style: TextInputStyle.Paragraph,
+                type: ComponentType.TextInput,
+              },
+            ],
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                custom_id: "links",
+                label: "Links",
+                max_length: 1_024,
+                min_length: 1,
+                placeholder: "The links for the bookmark separated by a space",
+                required: false,
+                style: TextInputStyle.Short,
+                type: ComponentType.TextInput,
+              },
+            ],
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                custom_id: "target-message",
+                label: "Target Message",
+                min_length: 16,
+                max_length: 20,
+                placeholder: "The id of the target message",
+                required: true,
+                style: TextInputStyle.Short,
+                type: ComponentType.TextInput,
+                value: message.id,
+              },
+            ],
+          },
+        ],
+      },
+    });
   },
 } as const;
